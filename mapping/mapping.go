05ca200e5651299f6832dcc7212ca0b5ba323b8e
ngicks <yknt.bsl@gmail.com>
2022-10-11 18:06:28 +0000

    refactor(mapping): add some helper type
    That can be directly unmarshalled from <es_host>/<index_name>/_mapping

--- mapping/mapping.go
+++ mapping/mapping.go
@@ -1,5 +1,27 @@
 package mapping
 
+// IndexSettings is main body for [Create index API.](https://www.elastic.co/guide/en/elasticsearch/reference/8.4/indices-create-index.html)
+type IndexSettings struct {
+	// Aliases is irrevant for this package's goal.
+	// But allowing it to be stored in here is maybe useful for some user.
+	Aliases any `json:"aliases,omitempty"`
+	// Settings is irrevant for this package's goal.
+	// But allowing it to be stored in here is maybe useful for some user.
+	Settings any       `json:"settings,omitempty"`
+	Mappings *Mappings `json:"mappings,omitempty"`
+}
+
+// MappingSettings is response body of GET /<index_name>/_mapping.
+//
+// Map may only contain <index_name> as a key, and the contained IndexSettings may only have Mappings.
+type MappingSettings map[string]IndexSettings
+
+// Mappings is main body for [updating mapping](https://www.elastic.co/guide/en/elasticsearch/reference/8.4/explicit-mapping.html#add-field-mapping),
+// or a part of [Create index API](https://www.elastic.co/guide/en/elasticsearch/reference/8.4/indices-create-index.html).
+type Mappings struct {
+	Properties Properties `json:"properties"`
+}
+
 type Properties map[string]any
 
 func (p *Properties) FillType() {

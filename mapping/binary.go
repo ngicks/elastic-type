b77d53c7d4c4bbc614ab0a032402ac1f78e428f8
ngicks <yknt.bsl@gmail.com>
2022-10-23 05:29:32 +0000

    doc(mapping): fix incorrect comment

--- mapping/binary.go
+++ mapping/binary.go
@@ -6,7 +6,7 @@ type BinaryParams struct {
 	Type esType `json:"type,omitempty"`
 	// DocValues indicates whether it should save field on disk in a column-stride fashion,
 	// so that it can later be used for sorting, aggregations, or scripting.
-	// Default(nil) is true.
+	// Default(nil) is false.
 	DocValues *bool `json:"doc_values,omitempty"`
 	// Store indicates whether the field value should be stored and retrievable separately from the _source field.
 	// Default is false.

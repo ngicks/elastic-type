9295acfaf5c9e69808d6c8af04596ca0804530a7
ngicks <yknt.bsl@gmail.com>
2022-10-23 05:02:29 +0000

    feat(es_type): add field helper type

---
+++ es_type/field.go
@@ -0,0 +1,118 @@
+package estype
+
+import "encoding/json"
+
+// IsEmpty determines if field would be treated as empty in Elastichsearch.
+// In its search context, null field is one of null, undefined (nonexistent), or an empty array.
+func IsEmpty[T any](val *[]T) bool {
+	if val == nil || len(*val) == 0 {
+		// len of nil slice is 0 anyway.
+		return true
+	}
+	return false
+}
+
+func IsNull[T any](val *[]T) bool {
+	return val != nil && *val == nil
+}
+
+func IsUndefined[T any](val *[]T) bool {
+	return val == nil
+}
+
+func UnwrapValue[T any](val *[]T) []T {
+	if *val == nil {
+		panic("nil slice")
+	}
+	return *val
+}
+
+// Field is an Elastichsearch field helper type.
+// A Field value can be null, undefined, T or an array of T.
+// It also can be a nested array but is not supported by this struct.
+// see: https://www.elastic.co/guide/en/elasticsearch/reference/8.4/array.html
+type Field[T any] struct {
+	inner *[]T
+	// If true, it marshals into an array even when its inner value slice is of single element or empty.
+	ShouldRetainArray bool
+}
+
+func (f Field[T]) IsNull() bool {
+	return IsNull(f.inner)
+}
+
+func (f Field[T]) IsUndefined() bool {
+	return IsUndefined(f.inner)
+}
+
+func (f Field[T]) IsEmpty() bool {
+	return IsEmpty(f.inner)
+}
+
+func (f *Field[T]) SetNull() {
+	var typedNil []T
+	f.inner = &typedNil
+}
+
+func (f *Field[T]) SetUndefined() {
+	f.inner = nil
+}
+
+func (f *Field[T]) SetValue(value []T) {
+	f.inner = &value
+}
+
+func (f *Field[T]) SetSingleValue(value T) {
+	f.inner = &[]T{value}
+}
+
+func (f Field[T]) Value() *[]T {
+	return f.inner
+}
+
+func (f Field[T]) ValueSingle() *T {
+	if f.inner == nil {
+		return nil
+	}
+	if len(*f.inner) > 0 {
+		return &(*f.inner)[0]
+	}
+	return nil
+}
+
+func (f Field[T]) Unwrap() []T {
+	return UnwrapValue(f.inner)
+}
+
+func (f Field[T]) UnwrapSingle() T {
+	return (*f.inner)[0]
+}
+
+func (f Field[T]) MarshalJSON() ([]byte, error) {
+	if f.IsUndefined() || f.IsNull() {
+		return []byte("null"), nil
+	}
+	if len(*f.inner) == 1 && !f.ShouldRetainArray {
+		return json.Marshal(f.UnwrapSingle())
+	}
+	return json.Marshal(*f.inner)
+}
+
+func (b *Field[T]) UnmarshalJSON(data []byte) error {
+	if data[0] == '[' {
+		return json.Unmarshal(data, b.inner)
+	}
+
+	if string(data) == "null" {
+		b.SetNull()
+		return nil
+	}
+
+	var single T
+	err := json.Unmarshal(data, &single)
+	if err != nil {
+		return err
+	}
+	b.SetSingleValue(single)
+	return nil
+}
---
+++ es_type/field_test.go
@@ -0,0 +1,167 @@
+package estype_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	estype "github.com/ngicks/elastic-type/es_type"
+	"github.com/stretchr/testify/require"
+)
+
+type testFieldStruct struct {
+	A estype.Field[string]         `json:"a"`
+	B estype.Field[estype.Boolean] `json:"b"`
+}
+
+func TestFieldMarshal(t *testing.T) {
+	require := require.New(t)
+
+	var bin []byte
+	var err error
+
+	var f testFieldStruct
+
+	f.A.SetNull()
+	f.B.SetNull()
+
+	bin, err = json.Marshal(f)
+	require.NoError(err)
+	require.Equal([]byte(`{"a":null,"b":null}`), bin)
+
+	f.A.SetUndefined()
+	f.B.SetUndefined()
+
+	bin, err = json.Marshal(f)
+	require.NoError(err)
+	// `undefined` is marshalled into null anyway. Upper structs must handle marshalling behavior.
+	require.Equal([]byte(`{"a":null,"b":null}`), bin)
+
+	f.A.SetValue([]string{"foo", "bar"})
+	f.B.SetSingleValue(estype.Boolean(true))
+
+	bin, err = json.Marshal(f)
+	require.NoError(err)
+	require.Equal([]byte(`{"a":["foo","bar"],"b":true}`), bin)
+
+	f.A.ShouldRetainArray = true
+	f.B.ShouldRetainArray = true
+
+	bin, err = json.Marshal(f)
+	require.NoError(err)
+	require.Equal([]byte(`{"a":["foo","bar"],"b":[true]}`), bin)
+}
+
+func TestFieldUnmarshal(t *testing.T) {
+	require := require.New(t)
+
+	var f testFieldStruct
+
+	err := json.Unmarshal([]byte(`{"a":null}`), &f)
+	require.NoError(err)
+
+	require.True(f.A.IsNull())
+	require.True(f.B.IsUndefined())
+
+	err = json.Unmarshal([]byte(`{"a":["foo","bar"],"b":true}`), &f)
+	require.NoError(err)
+
+	require.Condition(
+		func() bool { return cmp.Equal([]string{"foo", "bar"}, f.A.Unwrap()) },
+		cmp.Diff([]string{"foo", "bar"}, f.A.Unwrap()),
+	)
+	require.Condition(
+		func() bool { return cmp.Equal([]estype.Boolean{estype.Boolean(true)}, f.B.Unwrap()) },
+		cmp.Diff([]estype.Boolean{estype.Boolean(true)}, f.B.Unwrap()),
+	)
+
+	err = json.Unmarshal([]byte(`{"a":"foo","b":[true]}`), &f)
+	require.NoError(err)
+
+	require.Condition(
+		func() bool { return cmp.Equal([]string{"foo"}, f.A.Unwrap()) },
+		cmp.Diff([]string{"foo"}, f.A.Unwrap()),
+	)
+	require.Condition(
+		func() bool { return cmp.Equal([]estype.Boolean{estype.Boolean(true)}, f.B.Unwrap()) },
+		cmp.Diff([]estype.Boolean{estype.Boolean(true)}, f.B.Unwrap()),
+	)
+
+	err = json.Unmarshal([]byte(`{"a":123,"b":true}`), &f)
+	require.Error(err)
+
+	err = json.Unmarshal([]byte(`{"a":[123],"b":true}`), &f)
+	require.Error(err)
+}
+
+func TestFieldUtilities(t *testing.T) {
+	require := require.New(t)
+
+	var f testFieldStruct
+	testPanicOnUnwrap := func(shouldUnwrapPanic bool, shouldUnwrapSinglePanic bool) {
+		func() {
+			defer func() {
+				rec := recover()
+				if shouldUnwrapPanic {
+					require.NotNil(rec)
+				} else {
+					require.Nil(rec)
+				}
+			}()
+			f.A.Unwrap()
+		}()
+
+		func() {
+			defer func() {
+				rec := recover()
+				if shouldUnwrapSinglePanic {
+					require.NotNil(rec)
+				} else {
+					require.Nil(rec)
+				}
+			}()
+			f.A.UnwrapSingle()
+		}()
+	}
+
+	require.True(f.A.IsUndefined())
+	require.False(f.A.IsNull())
+	require.True(f.A.IsEmpty())
+	require.Nil(f.A.Value())
+	require.Nil(f.A.ValueSingle())
+	testPanicOnUnwrap(true, true)
+
+	f.A.SetNull()
+
+	require.False(f.A.IsUndefined())
+	require.True(f.A.IsNull())
+	require.True(f.A.IsEmpty())
+	testPanicOnUnwrap(true, true)
+
+	f.A.SetValue([]string{})
+
+	require.False(f.A.IsUndefined())
+	require.False(f.A.IsNull())
+	require.True(f.A.IsEmpty())
+	require.Equal(&[]string{}, f.A.Value())
+	require.Nil(f.A.ValueSingle())
+	testPanicOnUnwrap(false, true)
+
+	f.A.SetValue([]string{"foo", "bar"})
+
+	require.Condition(
+		func() bool { return cmp.Equal([]string{"foo", "bar"}, *f.A.Value()) },
+		cmp.Diff([]string{"foo", "bar"}, f.A.Unwrap()),
+	)
+	require.Equal("foo", *f.A.ValueSingle())
+	require.False(f.A.IsEmpty())
+	testPanicOnUnwrap(false, false)
+
+	f.A.SetSingleValue("baz")
+	require.Condition(
+		func() bool { return cmp.Equal([]string{"baz"}, f.A.Unwrap()) },
+		cmp.Diff([]string{"baz"}, f.A.Unwrap()),
+	)
+	require.Equal("baz", f.A.UnwrapSingle())
+	require.False(f.A.IsEmpty())
+}

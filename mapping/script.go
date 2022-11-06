520b80575a8516d37723fbb995bc349568e77dc7
ngicks <yknt.bsl@gmail.com>
2022-11-06 12:05:10 +0000

    feat(mapping): implement script

--- mapping/script.go
+++ mapping/script.go
@@ -1,8 +1,11 @@
 package mapping
 
-// Script is same as the definition of the runtime equivalent.
-// Structure of Script does not matter for this packages' goal.
-// So it is sipmly map here.
-//
-// TODO: implement
-type Script map[string]any
+// see https://www.elastic.co/guide/en/elasticsearch/reference/8.4/modules-scripting-using.html
+type Script struct {
+	// Defaults to "painless"
+	Lang *string `json:"lang,omitempty"`
+	// Use only either of source or id.
+	Source *string         `json:"source,omitempty"`
+	Id     *string         `json:"id,omitempty"`
+	Params *map[string]any `json:"params,omitempty"`
+}

b8510b09ec7dcd8722c48a487d2ba6b706abdd25
ngicks <yknt.bsl@gmail.com>
2022-11-10 18:11:26 +0000

    refactor(generate): use pre-generated aggregate metric

--- generate/aggregate_metric_double.go
+++ generate/aggregate_metric_double.go
@@ -1,17 +1,11 @@
 package generate
 
 import (
-	"bytes"
-	"html/template"
-
 	"github.com/ngicks/elastic-type/mapping"
 	"github.com/ngicks/type-param-common/slice"
 )
 
 func AggregateMetricDoubleParams(agg mapping.AggregateMetricDoubleParams, currentPointer slice.Deque[string]) GeneratedType {
-	fieldName, _ := currentPointer.PopBack()
-
-	buf := bytes.NewBuffer(make([]byte, 0))
 	var min, max, sum, valueCount bool
 
 	for _, v := range agg.Metrics {
@@ -27,36 +21,26 @@ func AggregateMetricDoubleParams(agg mapping.AggregateMetricDoubleParams, curren
 		}
 	}
 
-	err := aggregateMetricDoubleType.Execute(buf, aggregateMetricDoubleTypeParam{
-		Prefix:     capitalize(fieldName),
-		Min:        min,
-		Max:        max,
-		Sum:        sum,
-		ValueCount: valueCount,
-	})
-	if err != nil {
-		panic(err)
+	var suffix string
+	if min {
+		suffix += "Min"
+	}
+	if max {
+		suffix += "Max"
+	}
+	if sum {
+		suffix += "Sum"
+	}
+	if valueCount {
+		suffix += "ValueCount"
+	}
+
+	if min && max && sum && valueCount {
+		suffix = ""
 	}
 
 	return GeneratedType{
-		TyName: capitalize(fieldName) + "Agg",
-		TyDef:  buf.String(),
+		TyName:  "AggregateMetricDouble" + suffix,
+		Imports: estypeImport,
 	}
 }
-
-type aggregateMetricDoubleTypeParam struct {
-	Prefix     string
-	Min        bool
-	Max        bool
-	Sum        bool
-	ValueCount bool
-}
-
-var aggregateMetricDoubleType = template.Must(template.New("aggregateMetricDoubleType").Parse(`
-type {{.Prefix}}AggregateMetricDouble struct {
-{{ if .Min}}	Min        float64 ` + "`" + `json:"min"` + "`" + `{{ end }}
-{{ if .Max}}	Max        float64 ` + "`" + `json:"max"` + "`" + `{{ end }}
-{{ if .Sum}}	Sum        float64 ` + "`" + `json:"sum"` + "`" + `{{ end }}
-{{ if .ValueCount}}	ValueCount uint64  ` + "`" + `json:"value_count"` + "`" + `{{ end }}
-}
-`))

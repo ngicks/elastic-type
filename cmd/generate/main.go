30efc3b9af0d7d4b60942d8bcb347380891be272
ngicks <yknt.bsl@gmail.com>
2022-11-11 21:27:02 +0000

    feat(cmd/generate): add map option and global option load functionality

--- cmd/generate/main.go
+++ cmd/generate/main.go
@@ -11,10 +11,12 @@ import (
 )
 
 var (
-	pkgName = flag.String("pkg", "example", "package to generate.")
-	input   = flag.String("i", "--", "input filename. set -- if you want to read from stdin.")
-	outHigh = flag.String("out-high", "", "output filename to write high level types. panic if empty.")
-	outRaw  = flag.String("out-raw", "", "output filename to write raw types. panic if empty.")
+	pkgName       = flag.String("pkg", "example", "package to generate.")
+	input         = flag.String("i", "--", "input filename. set -- if you want to read from stdin.")
+	outHigh       = flag.String("out-high", "", "output filename to write high level types. panic if empty.")
+	outRaw        = flag.String("out-raw", "", "output filename to write raw types. panic if empty.")
+	mapOptPath    = flag.String("map-option", "", "path to a json file that can be unmarshalled to generate.MapOption.")
+	globalOptPath = flag.String("global-option", "", "path to a json file that con be unmarshalled to generate.GlobalOption. TypeNameGenerator field must be empty.")
 )
 
 func main() {
@@ -49,19 +51,44 @@ func main() {
 		panic(err)
 	}
 
+	mapOpt := generate.MapOption{}
+	globalOpt := generate.GlobalOption{}
+
+	if *mapOptPath != "" {
+		decode(*mapOptPath, &mapOpt)
+	}
+	if *globalOptPath != "" {
+		decode(*globalOptPath, &globalOpt)
+	}
+
 	indexName, mappings := getFirst(settings)
 
 	high, raw, err := generate.Generate(
 		mappings.Properties,
 		indexName,
-		generate.GlobalOption{},
-		generate.MapOption{},
+		globalOpt,
+		mapOpt,
 	)
 	if err != nil {
 		panic(err)
 	}
 
 	err = generate.WriteFile(*outHigh, *outRaw, high, raw, *pkgName)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func decode(filename string, v any) {
+	f, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	bin, err := io.ReadAll(f)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(bin, v)
 	if err != nil {
 		panic(err)
 	}

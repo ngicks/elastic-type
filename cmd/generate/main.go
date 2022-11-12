d72596a9ed752a6fe264012cfe9a73f941c80df1
ngicks <yknt.bsl@gmail.com>
2022-11-12 16:52:06 +0000

    feat(cmd/generate): add option to prefix type name with index name

--- cmd/generate/main.go
+++ cmd/generate/main.go
@@ -11,12 +11,40 @@ import (
 )
 
 var (
-	pkgName       = flag.String("pkg", "example", "package to generate.")
-	input         = flag.String("i", "--", "input filename. set -- if you want to read from stdin.")
-	outHigh       = flag.String("out-high", "", "output filename to write high level types. panic if empty.")
-	outRaw        = flag.String("out-raw", "", "output filename to write raw types. panic if empty.")
-	mapOptPath    = flag.String("map-option", "", "path to a json file that can be unmarshalled to generate.MapOption.")
-	globalOptPath = flag.String("global-option", "", "path to a json file that con be unmarshalled to generate.GlobalOption. TypeNameGenerator field must be empty.")
+	pkgName = flag.String("pkg", "example", "package to generate.")
+	input   = flag.String(
+		"i",
+		"--",
+		"input filename. set -- if you want to read from stdin. "+
+			"A content of the file must be what you can fetch from "+
+			"<elasticsearch origin>/<index_name>/_mappings",
+	)
+	outHigh = flag.String(
+		"out-high",
+		"",
+		"output filename to write high level types. panic if empty.",
+	)
+	outRaw = flag.String(
+		"out-raw",
+		"",
+		"output filename to write raw types. panic if empty.",
+	)
+	mapOptPath = flag.String(
+		"map-option",
+		"",
+		"path to a json file that can be unmarshalled to generate.MapOption.",
+	)
+	globalOptPath = flag.String(
+		"global-option",
+		"",
+		"path to a json file that con be unmarshalled to generate.GlobalOption. "+
+			"TypeNameGenerator field must be empty.",
+	)
+	prefixWithIndexName = flag.Bool(
+		"prefix-with-index-name",
+		false,
+		"whether it add prefix to every type name with index name.",
+	)
 )
 
 func main() {
@@ -54,14 +82,20 @@ func main() {
 	mapOpt := generate.MapOption{}
 	globalOpt := generate.GlobalOption{}
 
+	indexName, mappings := getFirst(settings)
+
 	if *mapOptPath != "" {
 		decode(*mapOptPath, &mapOpt)
 	}
 	if *globalOptPath != "" {
 		decode(*globalOptPath, &globalOpt)
 	}
-
-	indexName, mappings := getFirst(settings)
+	if *prefixWithIndexName {
+		globalOpt.TypeNameGenerator.PostProcess = []generate.TypeNamePostProcessRule{
+			generate.Prefix(indexName, 1),
+			generate.PascalCaseUnderscoreHyphen(),
+		}
+	}
 
 	high, raw, err := generate.Generate(
 		mappings.Properties,

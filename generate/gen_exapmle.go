0775a08984e03e37a1d4b45917edb2335f7f72e5
ngicks <yknt.bsl@gmail.com>
2022-11-12 17:10:34 +0000

    chore: move examples to test/

--- generate/gen_exapmle.go
+++
@@ -1,4 +0,0 @@
-package generate
-
-//go:generate go run ../cmd/generate/main.go -prefix-with-index-name -i ./example/object.json -out-high ./example/object_high.go -out-raw ./example/object_raw.go -map-option ./example/object_map_option.json
-//go:generate go run ../cmd/generate/main.go -prefix-with-index-name -i ./example/object_w_overlap.json -out-high ./example/object_w_overlap_high.go -out-raw ./example/object_w_overlap_raw.go

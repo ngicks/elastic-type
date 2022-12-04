c12f4a90f959115db8e0de8731a028e24a5b113a
ngicks <yknt.bsl@gmail.com>
2022-12-04 13:02:07 +0000

    refactor(generate): compact field generation

--- generate/field.go
+++ generate/field.go
@@ -25,25 +25,14 @@ func Field(
 	globalOpt GlobalOption,
 	opt FieldOption,
 ) (rawTy, testDef GeneratedType, err error) {
+	if rawTy, ok := fieldTypeTable[prop.Type]; ok {
+		return rawTy, GeneratedType{}, nil
+	}
+
 	switch prop.Type {
 	case mapping.AggregateMetricDouble:
 		gen := AggregateMetricDoubleParams(*prop.Param.(*mapping.AggregateMetricDoubleParams))
 		return gen, GeneratedType{}, nil
-	case mapping.Alias:
-		// FIXME: add special handling for this.
-		// Alias type needs knowledge about referenced field...
-		// Do nothing here?
-		return GeneratedType{
-				TyName: "any",
-			},
-			GeneratedType{},
-			nil
-	case mapping.Binary:
-		return GeneratedType{
-				TyName: "[]byte",
-			},
-			GeneratedType{},
-			nil
 	case mapping.Boolean:
 		var tyName string
 		if opt.PreferStringBoolean.True() {
@@ -55,12 +44,6 @@ func Field(
 		return GeneratedType{
 				TyName:  tyName,
 				Imports: estypeImport,
-			},
-			GeneratedType{},
-			nil
-	case mapping.Completion:
-		return GeneratedType{
-				TyName: "string",
 			},
 			GeneratedType{},
 			nil
@@ -76,156 +59,54 @@ func Field(
 			return GeneratedType{}, GeneratedType{}, err
 		}
 		return gen, testDef, nil
-	case mapping.DenseVector:
-		return GeneratedType{
-				// TODO: read dim and use an array instead of a slice?
-				TyName: "[]float64",
-			},
-			GeneratedType{},
-			nil
-	case mapping.Flattened:
-		return GeneratedType{
-				TyName: anyMap,
-			},
-			GeneratedType{},
-			nil
-	case mapping.Geopoint:
-		return GeneratedType{
-				TyName:  estypePrefix + "Geopoint",
-				Imports: estypeImport,
-			},
-			GeneratedType{},
-			nil
-	case mapping.Geoshape:
-		return GeneratedType{
-				TyName:  estypePrefix + "Geoshape",
-				Imports: estypeImport,
-			},
-			GeneratedType{},
-			nil
-	case mapping.IP:
-		return GeneratedType{
-				TyName:  "netip.Addr",
-				Imports: []string{`"net/netip"`},
-			},
-			GeneratedType{},
-			nil
-	case mapping.Histogram, mapping.Join, mapping.Percolator, mapping.Point:
-		// TODO: implement
-		return GeneratedType{
-				TyName: anyMap,
-			},
-			GeneratedType{},
-			nil
-	case mapping.RankFeature:
-		return GeneratedType{
-				TyName: "float64",
-			},
-			GeneratedType{},
-			nil
-	case mapping.RankFeatures:
-		return GeneratedType{
-				TyName: float64Map,
-			},
-			GeneratedType{},
-			nil
-	case mapping.SearchAsYouType:
-		return GeneratedType{
-				TyName: "string",
-			},
-			GeneratedType{},
-			nil
-	case mapping.Shape:
-		return GeneratedType{
-				TyName:  estypePrefix + "Geoshape",
-				Imports: estypeImport,
-			},
-			GeneratedType{},
-			nil
-	case mapping.TokenCount:
-		return GeneratedType{
-				TyName: "int64",
-			},
-			GeneratedType{},
-			nil
-	case mapping.Version:
-		// should this be sem ver package?
-		return GeneratedType{
-				TyName: "string",
-			},
-			GeneratedType{},
-			nil
-	case mapping.Keyword:
-		return GeneratedType{
-				TyName: "string",
-			},
-			GeneratedType{},
-			nil
-	case mapping.ConstantKeyword:
-		// The field can be stored if and only if value is same as specified in param.
-		return GeneratedType{
-				TyName: "string",
-			},
-			GeneratedType{},
-			nil
-	case mapping.Wildcard:
-		return GeneratedType{
-				TyName: "string",
-			},
-			GeneratedType{},
-			nil
-	case mapping.Text:
-		return GeneratedType{
-				TyName: "string",
-			},
-			GeneratedType{},
-			nil
-	case mapping.Long, mapping.Integer, mapping.Short, mapping.Byte, mapping.Double, mapping.Float, mapping.HalfFloat, mapping.UnsignedLong:
-		var tyName string
-		// https://www.elastic.co/guide/en/elasticsearch/reference/8.4/number.html
-		switch prop.Type {
-		case mapping.Long:
-			tyName = "int64"
-		case mapping.Integer:
-			tyName = "int32"
-		case mapping.Short:
-			tyName = "int16"
-		case mapping.Byte:
-			// The doc says it ranges -128 to 127. It's not the go built-in byte. Rather, it is a typical char type.
-			tyName = "int8"
-		case mapping.Double:
-			tyName = "float64"
-		case mapping.Float:
-			tyName = "float32"
-		case mapping.HalfFloat:
-			// there is not float16 type in built-in types.
-			// TODO: use float16 package?
-			tyName = "float32"
-		case mapping.UnsignedLong:
-			tyName = "uint64"
-		}
-
-		return GeneratedType{
-				TyName: tyName,
-			},
-			GeneratedType{},
-			nil
-	case mapping.ScaledFloat:
-		return GeneratedType{
-				TyName: "float64",
-			},
-			GeneratedType{},
-			nil
-	case mapping.IntegerRange, mapping.FloatRange, mapping.LongRange, mapping.DoubleRange, mapping.DateRange, mapping.IpRange:
-		// TODO: implement
-		// see https://www.elastic.co/guide/en/elasticsearch/reference/8.4/range.html
-		return GeneratedType{
-				TyName: anyMap,
-			},
-			GeneratedType{},
-			nil
 	}
 
 	// must not be reached
 	return GeneratedType{}, GeneratedType{}, fmt.Errorf("unknown type: %s", prop.Type)
+}
+
+var fieldTypeTable = map[mapping.EsType]GeneratedType{
+	// FIXME: add special handling for this.
+	// Alias type needs knowledge about referenced field...
+	// Do nothing here?
+	mapping.Alias:           {TyName: "any"},
+	mapping.Binary:          {TyName: "[]byte"},
+	mapping.Completion:      {TyName: "string"},
+	mapping.DenseVector:     {TyName: "[]float64"}, // TODO: read dim and use an array instead of a slice?
+	mapping.Flattened:       {TyName: anyMap},
+	mapping.Geopoint:        {TyName: estypePrefix + "Geopoint", Imports: estypeImport},
+	mapping.Geoshape:        {TyName: estypePrefix + "Geoshape", Imports: estypeImport},
+	mapping.IP:              {TyName: "netip.Addr", Imports: []string{`"net/netip"`}},
+	mapping.Histogram:       {TyName: anyMap}, // TODO: implement
+	mapping.Join:            {TyName: anyMap}, // TODO: implement
+	mapping.Percolator:      {TyName: anyMap}, // TODO: implement
+	mapping.Point:           {TyName: anyMap}, // TODO: implement
+	mapping.RankFeature:     {TyName: "float64"},
+	mapping.RankFeatures:    {TyName: float64Map},
+	mapping.SearchAsYouType: {TyName: "string"},
+	mapping.Shape:           {TyName: estypePrefix + "Geoshape", Imports: estypeImport},
+	mapping.TokenCount:      {TyName: "int64"},
+	mapping.Version:         {TyName: "string"}, // should this be sem ver package?
+	mapping.Keyword:         {TyName: "string"},
+	mapping.ConstantKeyword: {TyName: "string"}, // The field can be stored if and only if value is same as specified in param.
+	mapping.Wildcard:        {TyName: "string"},
+	mapping.Text:            {TyName: "string"},
+	// https://www.elastic.co/guide/en/elasticsearch/reference/8.4/number.html
+	mapping.Long:         {TyName: "int64"},
+	mapping.Integer:      {TyName: "int32"},
+	mapping.Short:        {TyName: "int16"},
+	mapping.Byte:         {TyName: "int8"}, // The doc says it ranges -128 to 127. It's not the go built-in byte. Rather, it is a typical char type.
+	mapping.Double:       {TyName: "float64"},
+	mapping.Float:        {TyName: "float32"},
+	mapping.HalfFloat:    {TyName: "float32"}, // TODO: use float16 package?
+	mapping.UnsignedLong: {TyName: "uint64"},
+	mapping.ScaledFloat:  {TyName: "float64"},
+	// TODO: implement
+	// see https://www.elastic.co/guide/en/elasticsearch/reference/8.4/range.html
+	mapping.IntegerRange: {TyName: anyMap},
+	mapping.FloatRange:   {TyName: anyMap},
+	mapping.LongRange:    {TyName: anyMap},
+	mapping.DoubleRange:  {TyName: anyMap},
+	mapping.DateRange:    {TyName: anyMap},
+	mapping.IpRange:      {TyName: anyMap},
 }
